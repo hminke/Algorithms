a105ed6ec73734440ba6526cebe32d663bea8b8d
Heather Minke <heather.a.minke@gmail.com>
2019-04-27 21:48:19 -0600

    Added data class to be able to have two items in each array index. Everything is displayed. Strings with correct gaps are not correct.

---
+++ Data.java
@@ -0,0 +1,20 @@
+
+public class Data {
+	
+	private int value;
+	private char direction;
+	
+	Data(int value, char direction) {
+		this.value = value;
+		this.direction = direction;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public char getDirection() {
+		return direction;
+	}
+
+}
--- Genetics.java
+++ Genetics.java
@@ -5,7 +5,7 @@ public class Genetics {
 
 	public static void main(String[] args) {
 
-		displayChart();
+		display();
 		
 	}
 	
@@ -27,7 +27,7 @@ public class Genetics {
 		return strings;
 	}
 	
-	public static int[][] createStringArray(String firstString, String secondString) {
+	public static Data[][] createArray(String firstString, String secondString) {
 		
 		int firstLength = 0;
 		int secondLength = 0;
@@ -37,25 +37,32 @@ public class Genetics {
 		firstLength = firstString.length();
 		secondLength = secondString.length();
 		
-		int[][] stringArray = new int[firstLength + 1][secondLength + 1];
+		Data[][] stringArray = new Data[firstLength + 1][secondLength + 1];
 			
 		for(int i = firstLength; i >= 0; i--) {
 			for(int j = secondLength; j >= 0; j--) {
 				if(i == firstLength) {
-					stringArray[i][j] = startingValue1;
+					int value = startingValue1;
+					char direction = '-';
+					stringArray[i][j] = new Data(value, direction);
 					startingValue1 += 2;
 				} else {
 					if(j == secondLength) {
-						stringArray[i][j] = startingValue2;
+						int value = startingValue2;
+						char direction = '-';
+						stringArray[i][j] = new Data(value, direction);
 						startingValue2 += 2;
+					} else {
+						stringArray[i][j] = new Data(0, '-');
 					}
 				}
 			}
 		}
+		
 		return stringArray;
 	}
 	
-	public static int diagonalBox(Character firstChar, Character secondChar, int[][] stringArray, int row, int column) {
+	public static int diagonalBox(Character firstChar, Character secondChar, Data[][] stringArray, int row, int column) {
 		int score = 0;
 		int temp = 0;
 		
@@ -65,14 +72,14 @@ public class Genetics {
 			score = 2;
 		}
 		
-		temp = stringArray[row + 1][column + 1];
+		temp = stringArray[row + 1][column + 1].getValue();
 	
 		score += temp;
 		
 		return score;
 	}
 	
-	public static int rightBox(Character secondChar, int[][] stringArray, int row, int column) {
+	public static int rightBox(Character secondChar, Data[][] stringArray, int row, int column) {
 		int score = 0;
 		int temp = 0;
 		Character firstChar = ' ';
@@ -83,14 +90,14 @@ public class Genetics {
 			score = 2;
 		}
 		
-		temp = stringArray[row][column + 1];
+		temp = stringArray[row][column + 1].getValue();
 	
 		score += temp;
 		
 		return score;
 	}
 	
-	public static int bottomBox(Character firstChar, int[][] stringArray, int row, int column) {
+	public static int bottomBox(Character firstChar, Data[][] stringArray, int row, int column) {
 		int score = 0;
 		int temp = 0;
 		Character secondChar = ' ';
@@ -101,36 +108,41 @@ public class Genetics {
 			score = 2;
 		}
 		
-		temp = stringArray[row + 1][column];
+		temp = stringArray[row + 1][column].getValue();
 	
 		score += temp;
 		
 		return score;
 	}
 	
-	public static int minFunc(int a, int b, int c) {
+	public static Data minFunc(int a, int b, int c) {
 		int min = 0;
+		char direction;
 		
 		if(a <= b && a <= c) {
 			min = a;
+			direction = 'D';
 		} else if(b < a && b <= c) {
 			min = b;
+			direction = 'R';
 		} else {
 			min = c;
+			direction = 'B';
 		}
 		
-		return min;
+		Data data = new Data(min, direction);
+		
+		return data;
 	}
 	
-	public static int[][] finalArray() {
+	public static Data[][] finalArray(String[] strings) {
 		
-		String[] strings = getStrings();
 		String firstString = strings[0];
 		String secondString = strings[1];
-		int[][] stringArray = createStringArray(firstString, secondString);
+		Data[][] stringArray = createArray(firstString, secondString);
 		int firstLength = firstString.length();
 		int secondLength = secondString.length();
-		int score = 0;
+		Data score;
 		
 		for(int i = firstLength - 1; i >= 0; i--) {
 			for(int j = secondLength - 1; j >= 0; j--) {
@@ -146,10 +158,67 @@ public class Genetics {
 		return stringArray;
 	}
 	
-	public static void displayChart() {
-		int[][] stringArray = finalArray();
+	public static String addSpace(char original) {
+		String newString = " " + original;
 		
-		System.out.print("\nDynamic Programming Chart: \n" + Arrays.deepToString(stringArray) + "\n");
+		return newString;
+	}
+	
+	public static void display() {
+		String[] strings = getStrings();
+		Data[][] stringArray = finalArray(strings);
+		String firstString = strings[0];
+		int firstLength = firstString.length();
+		String secondString = strings[1];
+		int secondLength = secondString.length();
+		String answer1 = "";
+		String answer2 = "";
+		char currentSquareDirection = stringArray[0][0].getDirection();
+		int g = 0;
+		int h = 0;
+		int i;
+		int j;
+		
+		System.out.print("\nDynamic Programming Chart: \n\nD = Diagonal\nR = Right\nB = Bottom\n\n");
+		
+		for(i = 0; i <= firstLength; i++) {
+			for(j = 0; j <= secondLength; j++) {
+				System.out.print(stringArray[i][j].getValue() + ":" + stringArray[i][j].getDirection() + " ");
+			}
+			
+			System.out.print("\n");
+		}
+		
+		i = 0;
+		j = 0;
+		
+		while(currentSquareDirection != '-') {
+			if(currentSquareDirection == 'D') {
+				answer1 = answer1 + firstString.charAt(g);
+				answer2 = answer2 + secondString.charAt(h);
+				i++;
+				j++;
+				g++;
+				h++;
+				currentSquareDirection = stringArray[i][j].getDirection();
+			} else if(currentSquareDirection == 'R') {
+				answer1 = answer1 + addSpace(firstString.charAt(g));
+				answer2 = answer2 + secondString.charAt(h);
+				i++;
+				h++;
+				g++;
+				currentSquareDirection = stringArray[i][j].getDirection();
+			} else if(currentSquareDirection == 'B' ) {
+				answer1 = answer1 + firstString.charAt(g);
+				answer2 = answer2 + addSpace(secondString.charAt(h));
+				j++;
+				g++;
+				h++;
+				currentSquareDirection = stringArray[i][j].getDirection();
+			}
+		}
+		
+		System.out.print("\nFirst String: " + answer1 + "\nSecondString: " + answer2 + "\nOptimal score: " + stringArray[0][0].getValue());
 		
 	}
 
